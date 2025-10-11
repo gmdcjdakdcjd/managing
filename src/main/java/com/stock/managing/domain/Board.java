529d0b0f16be6b740248fef34e978163206f8e4e
gmdcjdakdcjd <ysh010223@naver.com>
2025-10-11 22:10:59 +0900

    feat: JPA 및 QueryDSL 기반 매니징 페이지 기본 구조 추가

---
+++ src/main/java/com/stock/managing/domain/Board.java
@@ -0,0 +1,36 @@
+package com.stock.managing.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import jakarta.persistence.*;
+
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false) //칼럼의 길이와 null허용여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+    public void change(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
